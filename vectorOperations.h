ca3fba9b00705d4fdffe683bf7fd15bc43a6ee70
danielPalmor <danielpl@tauex.tau.ac.il>
2022-09-06 15:53:04 +0300

    comment check 2.0

--- vectorOperations.h
+++ vectorOperations.h
@@ -7,7 +7,6 @@ double dotProduct(double *point1, double *point2, int dim);
 void sumPoints(double* sum, double *point1,double *point2, int sign, int dim);
 double sumScalars(double* point, int dim);
 void multScalar(double *point, double scalar, int dim);
-//double calcDistance(int centroidIndex,int pointIndex, double **centroids,double **points, int dim);
 double calcDistance(double *point1,double *point2, int dim);
 
 #endif
