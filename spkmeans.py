5026baedba3f18107ff726d88594af79f73faf73
danielPalmor <danielpl@tauex.tau.ac.il>
2022-09-07 14:02:20 +0300

    spkmeans updates

--- spkmeans.py
+++ spkmeans.py
@@ -4,10 +4,16 @@ import numpy as np
 import pandas as pd
 import spectralkmeans
 
-np.random.seed(0)
-
 
 def kmeans_pp(k, goal, file_name):
+    """
+    This function performs the full normalized spectral clustering algorithm
+    :param k: The numbers of clusters
+    :param goal: The goal
+    :param file_name: The file that contains the points
+    :return: 0 for success, 1 for failure
+    """
+    np.random.seed(0)
     X = pd.read_csv(file_name, header=None)
     rows, columns = np.shape(X.values)
     X = np.reshape(X.values, newshape=X.size)
@@ -24,9 +30,9 @@ def kmeans_pp(k, goal, file_name):
             print("An Error Has Occurred")
             return 1
 
-        rand_indexes = pd.Series(T_as_df.index.values)
-        index = np.random.choice(T_as_df.index.values)
-        indexes = [rand_indexes[index]]
+        rand_indexes = pd.Series(T_as_df.index.values)  # The indexes to choose from randomly
+        index = np.random.choice(T_as_df.index.values)  # The chosen index
+        indexes = [rand_indexes[index]]  # The list of chosen indexes
 
         first_centroid = T_as_df.iloc[index]
         initial_centroids = [np.array(first_centroid)]
@@ -34,9 +40,10 @@ def kmeans_pp(k, goal, file_name):
         while i < k:
             list_of_p = np.array([])
             for point in T_as_df.values:
+                # Calculating the min distance of the current point from all centroids
                 d_point = min(np.dot(point - centroid, point - centroid) for centroid in initial_centroids)
                 list_of_p = np.append(list_of_p, d_point)
-            list_of_p = list_of_p / np.sum(list_of_p)
+            list_of_p = list_of_p / np.sum(list_of_p)  # Calculating the probabilities
             i += 1
 
             index = np.random.choice(T_as_df.index.values, p=list_of_p)
@@ -44,19 +51,21 @@ def kmeans_pp(k, goal, file_name):
             next_centroid = T_as_df.iloc[index]
             initial_centroids.append(next_centroid.values)
 
+        # Adjustments for the CAPI
         rows, columns = np.shape(initial_centroids)
         initial_centroids_as_list = np.reshape(initial_centroids, newshape=rows * columns)
         initial_centroids_as_list = [scalar for scalar in initial_centroids_as_list]
 
         the_centroids = spectralkmeans.kmeans_fit(k, result, initial_centroids_as_list,
                                                   len(initial_centroids[0]), len(T_as_df.values))
-
+        # Printing the chosen indexes
         for i in range(len(indexes)):
             if i == len(indexes) - 1:
                 print(int(indexes[i]))
             else:
                 print(int(indexes[i]), end=",")
 
+        # Printing the chosen centroids
         for i, point in enumerate(the_centroids):
             if (i + 1) % columns == 0:
                 print(format(point, ".4f"))
@@ -84,7 +93,6 @@ def kmeans_pp(k, goal, file_name):
 
 
 def main(argv):
-
     if len(argv) != 4 or not argv[1].isnumeric():
         print("Invalid Input!")
         return 1
